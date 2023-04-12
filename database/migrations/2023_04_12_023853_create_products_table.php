d721d8c2ae008a770f088d059e2611d83169d0d0
Fabrizio Ferroni <fabrizioferroni@outlook.com>
2023-04-11 23:47:37 -0300

    Se crearon las migraciones de las tablas categories y products

---
+++ database/migrations/2023_04_12_023853_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('description', 255);
+            $table->integer('stock');
+            $table->decimal('price', 16, 2);
+            $table->string('image_url', 150);
+            $table->foreignId('categories_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
