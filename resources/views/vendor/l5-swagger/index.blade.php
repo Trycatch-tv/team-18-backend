9ad9728d4aeadbf2adeedd1c3b35237464382a8b
Fabrizio Ferroni <fabrizioferroni@outlook.com>
2023-04-23 21:57:03 -0300

    Se cambio direccion de estilos en la vista

--- resources/views/vendor/l5-swagger/index.blade.php
+++ resources/views/vendor/l5-swagger/index.blade.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <title>{{config('l5-swagger.documentations.'.$documentation.'.api.title')}}</title>
-    <link rel="stylesheet" type="text/css" href="{{ l5_swagger_asset($documentation, 'swagger-ui.css') }}">
-    <link rel="icon" type="image/png" href="{{ l5_swagger_asset($documentation, 'favicon-32x32.png') }}" sizes="32x32"/>
-    <link rel="icon" type="image/png" href="{{ l5_swagger_asset($documentation, 'favicon-16x16.png') }}" sizes="16x16"/>
+    <link rel="stylesheet" type="text/css" href="http://api-inventario.onrender.com/swagger-api/swagger-ui.css">
+    {{-- <link rel="stylesheet" type="text/css" href="{{ l5_swagger_asset($documentation, 'swagger-ui.css') }}"> --}}
+    <link rel="icon" type="image/png" href="http://api-inventario.onrender.com/swagger-api/favicon-32x32.png" sizes="32x32"/>
+    {{-- <link rel="icon" type="image/png" href="{{ l5_swagger_asset($documentation, 'favicon-32x32.png') }}" sizes="32x32"/> --}}
+    <link rel="icon" type="image/png" href="http://api-inventario.onrender.com/swagger-api/favicon-16x16.png" sizes="16x16"/>
+    {{-- <link rel="icon" type="image/png" href="{{ l5_swagger_asset($documentation, 'favicon-16x16.png') }}" sizes="16x16"/> --}}
     <style>
         /* CLIENT-SPECIFIC STYLES */
 
@@ -69,8 +72,10 @@
 <body>
 <div id="swagger-ui"></div>
 
-<script src="{{ l5_swagger_asset($documentation, 'swagger-ui-bundle.js') }}"></script>
-<script src="{{ l5_swagger_asset($documentation, 'swagger-ui-standalone-preset.js') }}"></script>
+<script src="http://api-inventario.onrender.com/swagger-api/swagger-ui-bundle.js"></script>
+{{-- <script src="{{ l5_swagger_asset($documentation, 'swagger-ui-bundle.js') }}"></script> --}}
+<script src="http://api-inventario.onrender.com/swagger-api/swagger-ui-standalone-preset.js"></script>
+{{-- <script src="{{ l5_swagger_asset($documentation, 'swagger-ui-standalone-preset.js') }}"></script> --}}
 <script>
     window.onload = function() {
         // Build a system
