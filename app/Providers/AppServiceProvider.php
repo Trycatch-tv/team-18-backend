f573e725255426e40435fc3504feeca585057aa5
Fabrizio Ferroni <fabrizioferroni@outlook.com>
2023-04-23 22:28:56 -0300

    Se revirtieron cambios

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,10 +17,8 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(UrlGenerator $url)
+    public function boot(): void
     {
-        if (env('APP_ENV') == 'production') {
-            $url->forceScheme('https');
-        }
+        //
     }
 }
